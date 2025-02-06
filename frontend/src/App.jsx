8064981f44b5dbb457c8b4740338142e0eb4a8dc
aniketcoder003 <aniketcoder003@gmail.com>
2025-02-06 11:31:42 +0530

    intial frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Left from './Home/LeftPart/Left'
+import Right from './Home/RIghtPart/Right'
+
+ const App = () => {
+  return (
+    <div className='flex h-screen'>
+      <Left/>
+      <Right/>
+    </div>
+  )
+}
+
+export default App
